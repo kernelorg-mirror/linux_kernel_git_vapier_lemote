bf44215c9471c531ada7c1739424432e7a5289a3
Huacai Chen <chenhc@lemote.com>
2011-10-13 15:11:15 +0800

    Improve Loongson-3 SMP code
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/loongson/loongson-3a/smp.c
+++ arch/mips/loongson/loongson-3a/smp.c
@@ -23,36 +23,45 @@
 
 #include "smp.h"
 
-void loongson3_raw_writeq(unsigned long action, void * addr)   // write a value to mem 
+void loongson3_ipi_write64(uint64_t action, void * addr)   // write a value to mem 
 {                                                           // the value is action     
-    *((unsigned long *)addr) = action;
+	*((uint64_t *)addr) = action;
 };
 
-unsigned long loongson3_raw_readq(void * addr)                 // read a value from mem 
+uint64_t loongson3_ipi_read64(void * addr)                 // read a value from mem 
 {                                                           
-    return *((unsigned long *)addr);                         // the value will be return     
+	return *((uint64_t *)addr);                         // the value will be return     
 };
 
-static void *mailbox_set0_regs[] = {                        // addr for core_set0 reg,
-    (void *)(smp_core_group0_base + smp_core0_offset + SET0),              // which is a 32 bit reg
-    (void *)(smp_core_group0_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group0_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group0_base + smp_core3_offset + SET0),              // immediately                       
-    (void *)(smp_core_group1_base + smp_core0_offset + SET0),              // which is a 32 bit reg
-    (void *)(smp_core_group1_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group1_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group1_base + smp_core3_offset + SET0),              // immediately                       
-    (void *)(smp_core_group2_base + smp_core0_offset + SET0),              // which is a 32 bit reg
-    (void *)(smp_core_group2_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group2_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group2_base + smp_core3_offset + SET0),              // immediately                       
-    (void *)(smp_core_group3_base + smp_core0_offset + SET0),              // which is a 32 bit reg
-    (void *)(smp_core_group3_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group3_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group3_base + smp_core3_offset + SET0),              // immediately                       
+void loongson3_ipi_write32(uint32_t action, void * addr)   // write a value to mem 
+{                                                           // the value is action     
+	*((uint32_t *)addr) = action;
 };
 
-static void *mailbox_clear0_regs[] = {                      // addr for core_clear0 reg,
+uint32_t loongson3_ipi_read32(void * addr)                 // read a value from mem 
+{                                                           
+	return *((uint32_t *)addr);                         // the value will be return     
+};
+static void *ipi_set0_regs[] = {                        // addr for core_set0 reg,
+	(void *)(smp_core_group0_base + smp_core0_offset + SET0),              // which is a 32 bit reg
+	(void *)(smp_core_group0_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group0_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group0_base + smp_core3_offset + SET0),              // immediately                       
+	(void *)(smp_core_group1_base + smp_core0_offset + SET0),              // which is a 32 bit reg
+	(void *)(smp_core_group1_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group1_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group1_base + smp_core3_offset + SET0),              // immediately                       
+	(void *)(smp_core_group2_base + smp_core0_offset + SET0),              // which is a 32 bit reg
+	(void *)(smp_core_group2_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group2_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group2_base + smp_core3_offset + SET0),              // immediately                       
+	(void *)(smp_core_group3_base + smp_core0_offset + SET0),              // which is a 32 bit reg
+	(void *)(smp_core_group3_base + smp_core1_offset + SET0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group3_base + smp_core2_offset + SET0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group3_base + smp_core3_offset + SET0),              // immediately                       
+};
+
+static void *ipi_clear0_regs[] = {                      // addr for core_clear0 reg,
 	(void *)(smp_core_group0_base + smp_core0_offset + CLEAR0),            // which is a 32 bit reg
 	(void *)(smp_core_group0_base + smp_core1_offset + CLEAR0),            // When the bit of core_clear0 is 1,
 	(void *)(smp_core_group0_base + smp_core2_offset + CLEAR0),            // the bit of core_status0 become 0
@@ -71,7 +80,7 @@ static void *mailbox_clear0_regs[] = {                      // addr for core_cle
 	(void *)(smp_core_group3_base + smp_core3_offset + CLEAR0),            // immediately
 };
 
-static void *mailbox_regs0[] = {                            // addr for core_status0 reg
+static void *ipi_status_regs0[] = {                            // addr for core_status0 reg
 	(void *)(smp_core_group0_base + smp_core0_offset + STATUS0),           // which is a 32 bit reg
 	(void *)(smp_core_group0_base + smp_core1_offset + STATUS0),           // the reg is read only
 	(void *)(smp_core_group0_base + smp_core2_offset + STATUS0),
@@ -90,27 +99,27 @@ static void *mailbox_regs0[] = {                            // addr for core_sta
 	(void *)(smp_core_group3_base + smp_core3_offset + STATUS0),
 };
 
-static void *mailbox_en0_regs[] = {                        // addr for core_set0 reg,
-    (void *)(smp_core_group0_base + smp_core0_offset + EN0),              // which is a 32 bit reg
-    (void *)(smp_core_group0_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group0_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group0_base + smp_core3_offset + EN0),              // immediately                       
-    (void *)(smp_core_group1_base + smp_core0_offset + EN0),              // which is a 32 bit reg
-    (void *)(smp_core_group1_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group1_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group1_base + smp_core3_offset + EN0),              // immediately                       
-    (void *)(smp_core_group2_base + smp_core0_offset + EN0),              // which is a 32 bit reg
-    (void *)(smp_core_group2_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group2_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group2_base + smp_core3_offset + EN0),              // immediately                       
-    (void *)(smp_core_group3_base + smp_core0_offset + EN0),              // which is a 32 bit reg
-    (void *)(smp_core_group3_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
-    (void *)(smp_core_group3_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
-    (void *)(smp_core_group3_base + smp_core3_offset + EN0),              // immediately                       
+static void *ipi_en0_regs[] = {                        // addr for core_set0 reg,
+	(void *)(smp_core_group0_base + smp_core0_offset + EN0),              // which is a 32 bit reg
+	(void *)(smp_core_group0_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group0_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group0_base + smp_core3_offset + EN0),              // immediately                       
+	(void *)(smp_core_group1_base + smp_core0_offset + EN0),              // which is a 32 bit reg
+	(void *)(smp_core_group1_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group1_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group1_base + smp_core3_offset + EN0),              // immediately                       
+	(void *)(smp_core_group2_base + smp_core0_offset + EN0),              // which is a 32 bit reg
+	(void *)(smp_core_group2_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group2_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group2_base + smp_core3_offset + EN0),              // immediately                       
+	(void *)(smp_core_group3_base + smp_core0_offset + EN0),              // which is a 32 bit reg
+	(void *)(smp_core_group3_base + smp_core1_offset + EN0),              // When the bit of core_set0 is 1,
+	(void *)(smp_core_group3_base + smp_core2_offset + EN0),              // the bit of core_status0 become 1
+	(void *)(smp_core_group3_base + smp_core3_offset + EN0),              // immediately                       
 };
 
 
-static volatile void *mailbox_buf[] = {                              // addr for core_buf regs
+static volatile void *ipi_mailbox_buf[] = {                              // addr for core_buf regs
 	(void *)(smp_core_group0_base + smp_core0_offset + BUF),               // a group of regs with 0x40 byte size
 	(void *)(smp_core_group0_base + smp_core1_offset + BUF),               // which could be used for  
 	(void *)(smp_core_group0_base + smp_core2_offset + BUF),               // transfer args , r/w , uncached
@@ -154,48 +163,49 @@ void loongson3_timer_interrupt(struct pt_regs * regs)
 }
 
 /*
+ * Simple enough; everything is set up, so just poke the appropriate mailbox
+ * register, and we should be set
+ */
+static void loongson3_send_ipi_single(int cpu, unsigned int action)
+{
+	loongson3_ipi_write32((u32)action, ipi_set0_regs[cpu]);
+}
+
+static void loongson3_send_ipi_mask(const struct cpumask *mask, unsigned int action)
+{
+	unsigned int i;
+
+	for_each_cpu(i, mask)  
+		loongson3_send_ipi_single(i, action);
+}
+
+/*
  * SMP init and finish on secondary CPUs
  */
-void loongson3_smp_init(void)
+void loongson3_init_secondary(void)
 {
+	int i;
 	unsigned int imask = STATUSF_IP7 | STATUSF_IP6 | STATUSF_IP5 | 
                          STATUSF_IP4 | STATUSF_IP3 | STATUSF_IP2 ;
 		                 //STATUSF_IP1 | STATUSF_IP0;  // interrupt for software
-    int i;
 	/* Set interrupt mask, but don't enable */
 	change_c0_status(ST0_IM, imask);                   
 
-#if 1 /* gx */
-    printk("\n CPU#%d call smp_init!!!! \n", smp_processor_id());
-    for (i = 0; i < NR_CPUS; i++) {
-        loongson3_raw_writeq(0xffffffff, mailbox_en0_regs[i]);
-    }
-    printk("\n CPU#%d done smp_init en=%x!!!! \n",  smp_processor_id(), *(int *)(mailbox_en0_regs[smp_processor_id()]));
-#endif
+	printk("\n CPU#%d call init_secondary!!!! \n", smp_processor_id());
+	for (i = 0; i < NR_CPUS; i++) {
+		loongson3_ipi_write32(0xffffffff, ipi_en0_regs[i]);
+	}
+	printk("\n CPU#%d done init_secondary en=%x!!!! \n",  smp_processor_id(), *(int *)(ipi_en0_regs[smp_processor_id()]));
 }
 
 void loongson3_smp_finish(void)
 {
-      int tmp;
+	int tmp;
 
-      tmp = (read_c0_count() + 1000000);
-      write_c0_compare(tmp);
-      local_irq_enable();
-      printk("\n %s, CPU#%d CP0_ST=%x\n", __FUNCTION__, smp_processor_id(), read_c0_status());
-}
-
-/*
- * These are routines for dealing with the sb1250 smp capabilities
- * independent of board/firmware
- */
-
-/*
- * Simple enough; everything is set up, so just poke the appropriate mailbox
- * register, and we should be set
- */
-void core_send_ipi(int cpu, unsigned int action)
-{
-	loongson3_raw_writeq((u64)action, mailbox_set0_regs[cpu]);
+	tmp = (read_c0_count() + 1000000);
+	write_c0_compare(tmp);
+	local_irq_enable();
+	printk("\n %s, CPU#%d CP0_ST=%x\n", __FUNCTION__, smp_processor_id(), read_c0_status());
 }
 
 void loongson3_ipi_interrupt(struct pt_regs *regs)
@@ -209,79 +219,46 @@ void loongson3_ipi_interrupt(struct pt_regs *regs)
 	kstat_this_cpu.irqs[63]++;
 #endif    
 	/* Load the mailbox register to figure out what we're supposed to do */
-	action = loongson3_raw_readq(mailbox_regs0[cpu]);
+	action = loongson3_ipi_read32(ipi_status_regs0[cpu]);
     
 	/* Clear the mailbox to clear the interrupt */
-	loongson3_raw_writeq((u64)action,mailbox_clear0_regs[cpu]);
-	//loongson3_raw_writeq((u64)0xf,mailbox_clear0_regs[cpu]);
+	loongson3_ipi_write32((u32)action, ipi_clear0_regs[cpu]);
+	//loongson3_ipi_write32((u32)0xf, ipi_clear0_regs[cpu]);
 
 	/*
 	 * Nothing to do for SMP_RESCHEDULE_YOURSELF; returning from the
 	 * interrupt will do the reschedule for us
 	 */
-#if 0 /* gx */
-	if (action & SMP_CALL_FUNCTION)
-	  prom_printf("cpu#%d IPI action =%x\n", smp_processor_id(), action);
-#endif
 
 	if (action & SMP_CALL_FUNCTION) {
-	  smp_call_function_interrupt();
-
+		smp_call_function_interrupt();
 	}
-
-#if 0 /* gx for time */
-{
-extern unsigned int abscount;
-          if(!cpu) {
-	   if (action & 0x4)  abscount = read_c0_count();
-	    //while(1) printk("abscount=%u\n");
-	  }
-}	
-#endif
 }
+
 int loongson3_cpu_start(int cpu, void(*fn)(void), long sp, long gp, long a1)
 {
-    int res;
-    volatile unsigned long long startargs[4];
+	int res = 0;
+	volatile unsigned long long startargs[4];
 
-    startargs[0] = (long)fn;
-    startargs[1] = sp;
-    startargs[2] = gp;
-    startargs[3] = a1;
+	startargs[0] = (long)fn;
+	startargs[1] = sp;
+	startargs[2] = gp;
+	startargs[3] = a1;
 
-#if 0
-    prom_printf("\n writeq buf is begin ! \n");
-    prom_printf("\n CPU#%d mailbox_buf=%p ! \n", cpu, mailbox_buf[cpu]);
-    prom_printf("fn=%p\n", fn);
-    prom_printf("sp=%lx\n", sp);
-    prom_printf("gp=%lx\n", gp);
-    prom_printf("a1=%lx\n", a1);
-#endif   
-    
-    loongson3_raw_writeq(startargs[3], mailbox_buf[cpu]+0x18);
-    loongson3_raw_writeq(startargs[2], mailbox_buf[cpu]+0x10);
-    loongson3_raw_writeq(startargs[1], mailbox_buf[cpu]+0x8);
-    loongson3_raw_writeq(startargs[0], mailbox_buf[cpu]+0x0);
- 
-#if 0
-    prom_printf("\n readbackbuf is begin! \n");
-    prom_printf("fn=%p\n",  *((unsigned int *)(mailbox_buf[cpu]+0x0)));
-    prom_printf("sp=%lx\n", *((unsigned int *)(mailbox_buf[cpu]+0x8)));
-    prom_printf("gp=%lx\n", *((unsigned int *)(mailbox_buf[cpu]+0x10)));
-    prom_printf("a1=%lx\n", *((unsigned int *)(mailbox_buf[cpu]+0x18)));
-#endif   
-    res = 0;
+	loongson3_ipi_write64(startargs[3], (void*)(ipi_mailbox_buf[cpu]+0x18));
+	loongson3_ipi_write64(startargs[2], (void*)(ipi_mailbox_buf[cpu]+0x10));
+	loongson3_ipi_write64(startargs[1], (void*)(ipi_mailbox_buf[cpu]+0x8));
+	loongson3_ipi_write64(startargs[0], (void*)(ipi_mailbox_buf[cpu]+0x0));
 
-    return res;
+	return res;
 }
 
 int loongson3_cpu_stop(unsigned int i)
 {
-    return 0;
+	return 0;
 }
 
-//void __init plat_smp_setup(void)
-void __init loongson3_plat_smp_setup(void)
+void __init loongson3_smp_setup(void)
 {
 	int i, num;
 
@@ -304,7 +281,7 @@ void __init loongson3_plat_smp_setup(void)
 	printk(KERN_INFO "Detected %i available secondary CPU(s)\n", num);
 }
 
-void __init plat_prepare_cpus(unsigned int max_cpus)
+void __init loongson3_prepare_cpus(unsigned int max_cpus)
 {
 }
 
@@ -312,69 +289,33 @@ void __init plat_prepare_cpus(unsigned int max_cpus)
  * Setup the PC, SP, and GP of a secondary processor and start it
  * running!
  */
-void prom_boot_secondary(int cpu, struct task_struct *idle)
+void loongson3_boot_secondary(int cpu, struct task_struct *idle)
 {
 	int retval;
 
-    printk("\n BOOT CPU#%d...\n", cpu);
+	printk("\n BOOT CPU#%d...\n", cpu);
 	retval = loongson3_cpu_start(cpu_logical_map(cpu), &smp_bootstrap,     
 			       __KSTK_TOS(idle),                                   
 			       (unsigned long)task_thread_info(idle), 0);
 	if (retval != 0)
-		printk("!!!!!!!loongson3_start_cpu(%i) returned with err%i \n" , cpu, retval);
+		printk("!!!!!!loongson3_cpu_start(%i) returned with err%i \n" , cpu, retval);
 }
 
-/*
- * Code to run on secondary just after probing the CPU
- */
-void prom_init_secondary(void)
-{                 
-    extern void loongson3_smp_init(void);     
-    loongson3_smp_init();                    
-}
-
-/*
- * Do any tidying up before marking online and running the idle
- * loop
- */
-void prom_smp_finish(void)
-{
-    extern void loongson3_smp_finish(void);      
-    loongson3_smp_finish();                     
-}
 
 /*
  * Final cleanup after all secondaries booted
  */
-void prom_cpus_done(void)
+void loongson3_cpus_done(void)
 {
-}
-
-static void loongson3_send_ipi_single(int cpu, unsigned int action)
-{
-#if 0 /* gx */
-	prom_printf("---cpu=%d, action=0x%lx\n", cpu, action);
-	dump_stack();
-#endif
-    	loongson3_raw_writeq((((u64)action)), mailbox_set0_regs[cpu]);
-}
-
-//void loongson3_ipi_send_ipi_mask(cpumask_t mask, unsigned int action) //cww
-void loongson3_ipi_send_ipi_mask(const struct cpumask *mask, unsigned int action)
-{
-        unsigned int i;
-
-        for_each_cpu(i, mask)  
-                loongson3_send_ipi_single(i, action);
 }
 
 struct plat_smp_ops loongson3_smp_ops = {
-       .send_ipi_single = core_send_ipi,
-       .send_ipi_mask = loongson3_ipi_send_ipi_mask,
-       .init_secondary = prom_init_secondary,
-       .smp_finish = loongson3_smp_finish,
-       .cpus_done = prom_cpus_done,
-       .boot_secondary = prom_boot_secondary,
-       .smp_setup = loongson3_plat_smp_setup,
-       .prepare_cpus = plat_prepare_cpus,
+	.send_ipi_single = loongson3_send_ipi_single,
+	.send_ipi_mask = loongson3_send_ipi_mask,
+	.init_secondary = loongson3_init_secondary,
+	.smp_finish = loongson3_smp_finish,
+	.cpus_done = loongson3_cpus_done,
+	.boot_secondary = loongson3_boot_secondary,
+	.smp_setup = loongson3_smp_setup,
+	.prepare_cpus = loongson3_prepare_cpus,
 };
