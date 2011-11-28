6b1589ec2a6f6930b2f42b3bfbb65d952368f0c5
Chris Wilson <chris@chris-wilson.co.uk>
2011-11-28 21:10:05 +0000

    drm: Fix lack of CRTC disable for drm_crtc_helper_set_config(.fb=NULL)
        Disabling the CRTC by setting its framebuffer to NULL, as used by
        drm_framebuffer_cleanup(), was failing to pass the current framebuffer
        to the crtc_func->disable callback. This is because of the dance within
        drm_crtc_helper_set_config to pass the new_fb (NULL in this case) to the
        drm_crtc_helper_set_mode with the currently attached fb as a parameter.
        drm_crtc_helper_set_mode treats this as a no-op and the encoder is still
        enabled. And so the current fb is forgotten before the call to
        drm_helper_disable_unused_functions.
    
        This patch treats disabling the CRTC as a simple special case rather
        than adding further complexity into the configuration logic.
    
        This fixes a pin-leak of the fb bo on Xserver close.
    
        Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
        Signed-off-by: Dave Airlie <airlied@redhat.com>

--- drivers/gpu/drm/drm_crtc_helper.c
+++ drivers/gpu/drm/drm_crtc_helper.c
@@ -442,6 +442,29 @@ done:
 }
 EXPORT_SYMBOL(drm_crtc_helper_set_mode);
 
+static int
+drm_crtc_helper_disable(struct drm_crtc *crtc)
+{
+	struct drm_device *dev = crtc->dev;
+	struct drm_connector *connector;
+	struct drm_encoder *encoder;
+
+	/* Decouple all encoders and their attached connectors from this crtc */
+	list_for_each_entry(encoder, &dev->mode_config.encoder_list, head) {
+		if (encoder->crtc != crtc)
+			continue;
+
+		list_for_each_entry(connector, &dev->mode_config.connector_list, head) {
+			if (connector->encoder != encoder)
+				continue;
+
+			connector->encoder = NULL;
+		}
+	}
+
+	drm_helper_disable_unused_functions(dev);
+	return 0;
+}
 
 /**
  * drm_crtc_helper_set_config - set a new config from userspace
@@ -494,6 +517,7 @@ int drm_crtc_helper_set_config(struct drm_mode_set *set)
 		DRM_DEBUG_KMS("[CRTC:%d] [NOFB] #connectors=%d (x y) (%i %i)\n",
 				set->crtc->base.id, (int)set->num_connectors,
 				set->x, set->y);
+		return drm_crtc_helper_disable(set->crtc);
 	}
 
 	dev = set->crtc->dev;
