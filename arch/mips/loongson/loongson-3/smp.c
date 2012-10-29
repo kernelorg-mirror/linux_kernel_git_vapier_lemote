868fcce2d255ad81ca643f83da4b7e7bbbb64428
Huacai Chen <chenhc@lemote.com>
2012-10-29 13:56:34 +0800

    Spurious IRQ fixes for Loongson-3
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/loongson/loongson-3/smp.c
+++ arch/mips/loongson/loongson-3/smp.c
@@ -211,9 +211,8 @@ void __cpuinit loongson3_init_secondary(void)
 	int i;
 	uint32_t initcount;
  	unsigned int cpu = smp_processor_id();	
-	unsigned int imask = STATUSF_IP7 | STATUSF_IP6 | STATUSF_IP5 | 
-                         STATUSF_IP4 | STATUSF_IP3 | STATUSF_IP2 ;
-		                 //STATUSF_IP1 | STATUSF_IP0;  // interrupt for software
+	unsigned int imask = STATUSF_IP7 | STATUSF_IP6 |
+                            STATUSF_IP3 | STATUSF_IP2;
 
 	/* Set interrupt mask, but don't enable */
 	change_c0_status(ST0_IM, imask);                   
