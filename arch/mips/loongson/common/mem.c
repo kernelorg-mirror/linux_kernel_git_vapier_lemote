bdc1edf1d405390a6509351e41e2122c26e93086
Huacai Chen <chenhc@lemote.com>
2013-04-28 14:19:58 +0800

    Loongson: Improve SMBIOS memory region init
    Signed-off-by: Huacai Chen <chenhc@lemote.com>

--- arch/mips/loongson/common/mem.c
+++ arch/mips/loongson/common/mem.c
@@ -42,8 +42,9 @@ void __init prom_init_memory(void)
 				break;
 			case SMBIOS_TABLE:
 				has_smbios = 1;
-				smbios_addr = emap->map[i].mem_start &
-					0x000000000ffffffful;
+				smbios_addr = emap->map[i].mem_start;
+				add_memory_region(emap->map[i].mem_start,
+					0x2000, BOOT_MEM_RESERVED);
 				break;
 			}
 		}
